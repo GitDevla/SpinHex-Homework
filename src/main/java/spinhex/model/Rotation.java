05f19081a6fbd2c907ec93f7b6abdff52d964cad
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-05-27 15:38:31 +0200

    refactor: add documentation to Rotation enum

--- src/main/java/spinhex/model/Rotation.java
+++ src/main/java/spinhex/model/Rotation.java
@@ -1,5 +1,15 @@
 package spinhex.model;
 
+/**
+ * Represents the direction of rotation in the SpinHex game.
+ * <p>
+ * This enum defines two rotation directions:
+ * <ul>
+ *   <li>{@code CLOCKWISE} - Rotation in the clockwise direction</li>
+ *   <li>{@code COUNTERCLOCKWISE} - Rotation in the counterclockwise direction</li>
+ * </ul>
+ * </p>
+ */
 public enum Rotation {
     CLOCKWISE,
     COUNTERCLOCKWISE
