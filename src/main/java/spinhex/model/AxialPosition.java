0b5ec36464c644e8e3208d4214386799591effad
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-05-27 13:27:47 +0200

    feat: add arithmetic and equality methods to AxialPosition

--- src/main/java/spinhex/model/AxialPosition.java
+++ src/main/java/spinhex/model/AxialPosition.java
@@ -6,4 +6,16 @@ public record AxialPosition(int q, int s) {
     public String toString() {
         return String.format("(%d,%d)", q, s);
     }
+
+    public AxialPosition add(AxialPosition other) {
+        return new AxialPosition(this.q + other.q, this.s + other.s);
+    }
+
+    public AxialPosition subtract(AxialPosition other) {
+        return new AxialPosition(this.q - other.q, this.s - other.s);
+    }
+
+    public boolean isEqual(AxialPosition other) {
+        return this.q == other.q && this.s == other.s;
+    }
 }
