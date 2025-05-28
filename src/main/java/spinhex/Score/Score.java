fb91c5b9f946d531a443ec4966634a3ae994f9c8
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-05-28 16:00:45 +0200

    docs: add javadoc to Score

--- src/main/java/spinhex/Score/Score.java
+++ src/main/java/spinhex/Score/Score.java
@@ -2,22 +2,47 @@ package spinhex.Score;
 
 import java.time.ZonedDateTime;
 
+/**
+ * Represents a score in the game, associating a username with a numeric score.
+ * This class is used to track player performance.
+ */
 public class Score {
     private String username;
     private int score;
 
+    /**
+     * Default constructor that creates an empty Score object.
+     * This constructor should be only used implicitly by Jackson for
+     * deserialization.
+     */
     public Score() {
     }
 
+    /**
+     * Creates a Score object with the specified username and score.
+     *
+     * @param username the player's username
+     * @param score    the player's numeric score
+     */
     public Score(String username, int score) {
         this.username = username;
         this.score = score;
     }
 
+    /**
+     * Returns the username associated with this score.
+     *
+     * @return the player's username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the numeric score value.
+     *
+     * @return the player's score
+     */
     public int getScore() {
         return score;
     }
