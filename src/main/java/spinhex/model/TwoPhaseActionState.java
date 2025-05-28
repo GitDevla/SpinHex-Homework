f639ba04037380d465ad501443df4c0650a70b0c
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-05-28 15:57:59 +0200

    docs: add javadoc to TwoPhaseActionState

--- src/main/java/spinhex/model/TwoPhaseActionState.java
+++ src/main/java/spinhex/model/TwoPhaseActionState.java
@@ -2,10 +2,40 @@ package spinhex.model;
 
 import puzzle.State;
 
-public interface TwoPhaseActionState<T,U> extends State<TwoPhaseActionState.TwoPhaseAction<T,U>> {
+/**
+ * Represents a state in a two-phase action system where actions are performed
+ * in two distinct phases: first selecting a source element, then specifying
+ * the action to perform from that source.
+ *
+ * <p>
+ * This interface extends the State interface with a specialized action type
+ * that encapsulates both the source element and the action to be performed.
+ * </p>
+ *
+ * @param <T> the type of the source element (from where an action originates)
+ * @param <U> the type of the action to be performed
+ * 
+ * @see State
+ */
+public interface TwoPhaseActionState<T, U> extends State<TwoPhaseActionState.TwoPhaseAction<T, U>> {
 
+    /**
+     * Determines whether it is legal to initiate an action from the specified
+     * source.
+     *
+     * @param from the source element to check
+     * @return {@code true} if an action can be initiated from the specified source,
+     *         {@code false} otherwise
+     */
     boolean isLegalToMoveFrom(T from);
 
-    record TwoPhaseAction<T,U>(T from, U action) {}
-
+    /**
+     * Represents a two-phase action consisting of a source element and the action
+     * to be performed.
+     *
+     * @param <T> the type of the source element
+     * @param <U> the type of the action
+     */
+    record TwoPhaseAction<T, U>(T from, U action) {
+    }
 }
