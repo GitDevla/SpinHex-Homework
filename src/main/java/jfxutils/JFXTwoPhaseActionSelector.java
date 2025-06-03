c67f1228bbc8c720d5b8ca791304654eb43e6268
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-06-03 19:56:36 +0200

    docs: add javadoc to JFXTwoPhaseActionSelector and TwoPhaseActionSelector

--- src/main/java/jfxutils/JFXTwoPhaseActionSelector.java
+++ src/main/java/jfxutils/JFXTwoPhaseActionSelector.java
@@ -4,14 +4,30 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import spinhex.model.TwoPhaseActionState;
 
-public class JFXTwoPhaseActionSelector<T,U> extends TwoPhaseActionSelector<T,U> {
+/**
+ * A subclass of {@link TwoPhaseActionSelector} that provides a
+ * property to observe the selection phase.
+ *
+ * @param <T> represents the moves that can be selected
+ * @param <U> represents the actions that can be applied to the moves
+ */
+public class JFXTwoPhaseActionSelector<T, U> extends TwoPhaseActionSelector<T, U> {
     private final ReadOnlyObjectWrapper<Phase> phaseProperty;
 
-    public JFXTwoPhaseActionSelector(TwoPhaseActionState<T,U> state) {
+    /**
+     * Creates a {@code JFXTwoPhaseActionSelector} object to determine the next
+     * move in the state specified.
+     * 
+     * @param state the state in which the next move is to be made
+     */
+    public JFXTwoPhaseActionSelector(TwoPhaseActionState<T, U> state) {
         super(state);
         this.phaseProperty = new ReadOnlyObjectWrapper(this.phase);
     }
 
+    /**
+     * Represents the current selection phase.
+     */
     public ReadOnlyObjectProperty<Phase> phaseProperty() {
         return this.phaseProperty.getReadOnlyProperty();
     }
--- src/main/java/jfxutils/TwoPhaseActionSelector.java
+++ src/main/java/jfxutils/TwoPhaseActionSelector.java
@@ -2,31 +2,87 @@ package jfxutils;
 
 import spinhex.model.TwoPhaseActionState;
 
+/**
+ * Utility class to determine the next action in two-phase action selection
+ * puzzles.
+ * It manages the state transitions between selecting a source position and
+ * selecting an action to apply at that position.
+ *
+ * @param <T> the type representing the source position (from)
+ * @param <U> the type representing the action to apply
+ */
 public class TwoPhaseActionSelector<T, U> {
+
+    /**
+     * Represents the current phase of the action selection process.
+     */
+    public static enum Phase {
+        /**
+         * Phase where the user selects the source position.
+         */
+        SELECT_FROM,
+
+        /**
+         * Phase where the user selects which action to apply at the selected position.
+         */
+        SELECT_ACTION,
+
+        /**
+         * Phase where both source position and action have been selected and the move
+         * is ready to be executed.
+         */
+        READY_TO_MOVE
+    }
+
     private final TwoPhaseActionState<T, U> state;
     protected TwoPhaseActionSelector.Phase phase;
     private boolean invalidSelection;
     private T from;
     private U action;
 
+    /**
+     * Creates a {@code TwoPhaseActionSelector} to manage the action selection
+     * process for the given state.
+     *
+     * @param state the state on which actions will be performed
+     */
     public TwoPhaseActionSelector(TwoPhaseActionState<T, U> state) {
         this.state = state;
         this.phase = TwoPhaseActionSelector.Phase.SELECT_FROM;
         this.invalidSelection = false;
     }
 
+    /**
+     * {@return the current selection phase}
+     */
     public final TwoPhaseActionSelector.Phase getPhase() {
         return this.phase;
     }
 
+    /**
+     * Sets the current selection phase. The method is provided to be overridden
+     * by subclasses.
+     *
+     * @param phase the current selection phase
+     */
     protected void setPhase(TwoPhaseActionSelector.Phase phase) {
         this.phase = phase;
     }
 
+    /**
+     * {@return whether the move is ready to be made}
+     */
     public final boolean isReadyToMove() {
         return this.phase == TwoPhaseActionSelector.Phase.READY_TO_MOVE;
     }
 
+    /**
+     * Selects a source position from which an action will be performed.
+     * If the position is valid for making a move, the selection process advances
+     * to the action selection phase.
+     * 
+     * @param from the position to select as the source
+     */
     public void selectFrom(T from) {
         if (this.state.isLegalToMoveFrom(from)) {
             this.from = from;
@@ -35,20 +91,30 @@ public class TwoPhaseActionSelector<T, U> {
         } else {
             this.invalidSelection = true;
         }
-
     }
 
+    /**
+     * Selects an action to be performed at the previously selected source position.
+     * If the action is valid for the selected source, the selection process
+     * advances to the ready-to-move phase.
+     * 
+     * * @param action the action to perform at the selected source
+     */
     public void selectAction(U action) {
-        if (this.state.isLegalMove(new TwoPhaseActionState.TwoPhaseAction(this.from, action))) {
+        if (this.state.isLegalMove(new TwoPhaseActionState.TwoPhaseAction<>(this.from, action))) {
             this.action = action;
             this.setPhase(TwoPhaseActionSelector.Phase.READY_TO_MOVE);
             this.invalidSelection = false;
         } else {
             this.invalidSelection = true;
         }
-
     }
 
+    /**
+     * {@return the source position selected} If still in the SELECT_FROM phase,
+     * then an {@link IllegalStateException} is thrown as no source has been
+     * selected yet.
+     */
     public final T getFrom() {
         if (this.phase == TwoPhaseActionSelector.Phase.SELECT_FROM) {
             throw new IllegalStateException();
@@ -57,6 +123,10 @@ public class TwoPhaseActionSelector<T, U> {
         }
     }
 
+    /**
+     * {@return the action selected} If the selection is not in the READY_TO_MOVE
+     * phase, then an {@link IllegalStateException} is thrown.
+     */
     public final U getAction() {
         if (this.phase != TwoPhaseActionSelector.Phase.SELECT_FROM
                 && this.phase != TwoPhaseActionSelector.Phase.SELECT_ACTION) {
@@ -66,19 +136,31 @@ public class TwoPhaseActionSelector<T, U> {
         }
     }
 
+    /**
+     * {@return whether the last selection (i.e, for the source or the target,
+     * respectively) was invalid}
+     */
     public final boolean isInvalidSelection() {
         return this.invalidSelection;
     }
 
+    /**
+     * Makes the move selected. If the move is not yet ready to be made, then an
+     * {@link IllegalStateException} is thrown.
+     */
     public final void makeMove() {
         if (this.phase != TwoPhaseActionSelector.Phase.READY_TO_MOVE) {
             throw new IllegalStateException();
         } else {
-            this.state.makeMove(new TwoPhaseActionState.TwoPhaseAction(this.from, this.action));
+            this.state.makeMove(new TwoPhaseActionState.TwoPhaseAction<>(this.from, this.action));
             this.reset();
         }
     }
 
+    /**
+     * Resets the selection state, clearing both the source position and action
+     * selections.
+     */
     public final void reset() {
         this.from = null;
         this.action = null;
@@ -86,12 +168,4 @@ public class TwoPhaseActionSelector<T, U> {
         this.invalidSelection = false;
     }
 
-    public static enum Phase {
-        SELECT_FROM,
-        SELECT_ACTION,
-        READY_TO_MOVE;
-
-        private Phase() {
-        }
-    }
 }
