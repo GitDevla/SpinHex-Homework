262d2f6e14ba544b39abd8b834e3291b0b38a7f2
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-06-16 13:49:15 +0200

    chore: import original BFS solver

---
+++ src/main/java/solver/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Package for solving puzzles.
+ * Modified from the original homework project utils.
+ * 
+ * @see <a href=
+ *      "https://github.com/INBPM0420L/homework-project-utils-2025">homework-project-utils-2025</a>
+ */
+package solver;
